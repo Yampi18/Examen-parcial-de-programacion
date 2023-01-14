79fc6cb7a3bddea75f269cdceacd74c6f7a8abcd
Yampi18 <jeanpierre18_10hotmail.com>
2023-01-14 11:55:29 -0500

    primera version

---
+++ parcial.js
@@ -0,0 +1,10 @@
+var num = Number(prompt("Ingrese el primer numero"));
+var num2 = Number(prompt("Ingrese el segundo numero"));
+
+while(num<0 && num2<0){
+    var i = i+num2+ num
+    var a = a*num*num2
+
+}
+
+window.alert("Los numeros son: ", i);
---
+++ parcial.py
@@ -0,0 +1,13 @@
+num = int(input("Ingrese el primer numero : "))
+num2 = int(input("Ingrese el segundo numero : "))
+mul= num*num2
+
+i=1
+a=1
+while(num>0 & num2>0):
+    i=i+num
+    a=a*mul
+    
+    
+print("los numeros son: ", i )
+print("la multilpicacion es: ", a)
